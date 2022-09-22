e7e5f29e7436a8f96fa074fc7d8c551b8e0e20ed
vray797 <vray797@gmail.com>
2022-09-22 18:12:42 +0530

    key value API call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Frontend';
+  constructor(private http: HttpClient) {}
+
+  data: any
+
+  getData(){
+    return this.http.get("https://api.publicapis.org/entries").subscribe((res) => {
+      this.data = res
+      console.log(this.data)
+    })
+  }
+
+  ngOnInit() {
+    this.getData()
+  }
+
 }
